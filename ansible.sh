c40b77227911086c9bd29c7510144b396c9c050f
Sowmyagummalla <sowmyareddygummalla@gmail.com>
2024-08-25 19:16:04 +0530

    Ansible Installation

---
+++ ansible.sh
@@ -0,0 +1,9 @@
+ANSIBLE INSTALLATION IN UBUNTU
+
+ sudo apt update
+ sudo apt install software-properties-common
+ sudo add-apt-repository --yes --update ppa:ansible/ansible
+ sudo apt install ansible -y
+
+
+ ANSIBLE INSTALLATION IN LINUX
